45f1badc32eed0042a10f4639329ce90321c8d55
Max-dot668 <maxiyouit@gmail.com>
2025-08-11 19:36:56 -0700

    Store tags in an Enum for reusability

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import Annotated
 from fastapi import FastAPI,Path, Query, status, Form, File, UploadFile, HTTPException
 from pydantic import BaseModel, Field, HttpUrl, EmailStr
@@ -34,6 +35,10 @@ class FilterParams(BaseModel):
     name: str | None = None
     priority: int | None = None
 
+class Tags(Enum):
+    items = "items"
+    users = "users"
+
 items = {} 
 
 def fake_password_hasher(raw_password: str) -> str:
@@ -49,19 +54,19 @@ def fake_save_user(user_in: UserIn) -> UserInDB:
 async def root() -> dict:
     return {"message": "groceries list manager"}
 
-@app.post("/login/", tags=["users"], status_code=status.HTTP_201_CREATED)
+@app.post("/login/", tags=[Tags.users], status_code=status.HTTP_201_CREATED)
 async def login(form_data: Annotated[FormData, Form()]):
     return form_data.username
 
-@app.post("/user/", response_model_exclude_unset=True, tags=["users"], status_code=status.HTTP_201_CREATED)
+@app.post("/user/", response_model_exclude_unset=True, tags=[Tags.users], status_code=status.HTTP_201_CREATED)
 async def create_user(user: UserIn) -> BaseUser:
     return user 
 
-@app.get("/user/items", tags=["users"])
+@app.get("/user/items", tags=[Tags.users])
 async def read_items() -> dict:
     return items
 
-@app.get("/user/items/item", tags=["users"])
+@app.get("/user/items/item", tags=[Tags.users])
 async def read_item(filter: Annotated[FilterParams, Query()]) -> list:
     result = []
     for id in items:
@@ -69,14 +74,14 @@ async def read_item(filter: Annotated[FilterParams, Query()]) -> list:
             result.append({"name": items[id]["name"], "quantity": items[id]["quantity"]})
     return result
         
-@app.post("/user/items/{item_id}", response_model_exclude_unset=True, tags=["items"], status_code=status.HTTP_201_CREATED)
+@app.post("/user/items/{item_id}", response_model_exclude_unset=True, tags=[Tags.items], status_code=status.HTTP_201_CREATED)
 async def create_item(item_id: Annotated[int, Path()], item: Item,) -> dict:
     item_data = item.model_dump()
     item_data.update({"date": datetime.now()})
     items[item_id] = item_data
     return {"item added": item_data}
 
-@app.put("/user/items/{item_id}", response_model_exclude_unset=True, tags=["items"])
+@app.put("/user/items/{item_id}", response_model_exclude_unset=True, tags=[Tags.items])
 async def update_item(item_id: Annotated[int, Path()], item: Item) -> dict:
     if item_id not in items:
         raise HTTPException(status_code=404, detail="Item not found")
@@ -84,7 +89,7 @@ async def update_item(item_id: Annotated[int, Path()], item: Item) -> dict:
         items[item_id] = item.model_dump()
     return {"message": "item was updated"}
 
-@app.post("/files/", tags=["files"], status_code=status.HTTP_201_CREATED)
+@app.post("/files/", tags=[Tags.items], status_code=status.HTTP_201_CREATED)
 async def create_upload_file(
     file: Annotated[bytes, File()],
     fileb: Annotated[UploadFile, File()],
